75419f4d061399be4af31537b2836428c6765093
Jan Diehm <jadiehm@gmail.com>
2024-10-17 10:16:50 -0500

    full url address

--- src/components/Meta.svelte
+++ src/components/Meta.svelte
@@ -19,7 +19,7 @@
 	<meta property="og:type" content="article" />
 	<meta property="og:locale" content="en_US" />
 
-	<meta property="og:image" content="{url}/assets/social/social-facebook.jpg" />
+	<meta property="og:image" content="./assets/social/social-facebook.jpg" />
 	<meta property="og:image:type" content="image/jpeg" />
 	<meta property="og:image:width" content="1200" />
 	<meta property="og:image:height" content="600" />
@@ -29,7 +29,7 @@
 	<meta name="twitter:creator" content="" />
 	<meta name="twitter:title" content="A F A N G I N T H E R O U G H" />
 	<meta name="twitter:description" content="A concept album in 3 parts by A N O T H E R W I L D" />
-	<meta name="twitter:image:src" content="{url}/assets/social/social-twitter.jpg" />
+	<meta name="twitter:image:src" content="./assets/social/social-twitter.jpg" />
 
 	<meta name="robots" content="max-image-preview:large" />
 
