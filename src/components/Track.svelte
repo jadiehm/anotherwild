b9da45ff0c2a1843c8dc1574eb7a5b96260583c8
Jan Diehm <jadiehm@gmail.com>
2024-10-17 18:42:59 -0500

    track cuts

--- src/components/Track.svelte
+++ src/components/Track.svelte
@@ -29,14 +29,14 @@
             // Change the video source
 
             if ($currTrack !== null) {
-                audioSource.src = `assets/audio/preview/${$currTrack}.mp3`;
+                audioSource.src = `assets/audio/${$currTrack}.mp3`;
                 source.src = `assets/video/${$currTrack}.mp4`;
                 video.setAttribute("preload", "none");
                 video.load();
                 video.muted = true; // Ensures the video remains muted
                 video.play();
             } else {
-                audioSource.src = `assets/video/01FLAGGROUND.mp3`;
+                audioSource.src = `assets/audio/01FLAGGROUND.mp3`;
                 source.src = `assets/video/01FLAGGROUND.mp4`;
                 video.setAttribute("preload", "none");
                 video.load();
