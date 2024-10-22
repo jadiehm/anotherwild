5bb8d24de540d1fbcb44f65b7fd85d148861aa96
Jan Diehm <jadiehm@gmail.com>
2024-10-22 10:10:46 -0500

    text updates

--- src/components/Info.svelte
+++ src/components/Info.svelte
@@ -68,7 +68,7 @@
 
         <!-- Page 1 -->
         <div class="page-wrapper">
-            <div class="page" style="transform: rotate(-1deg)">
+            <div class="page" style="transform: rotate(0.5deg)">
                 {#each copy.note2 as graf, i}
                     <p>{@html graf.value}</p>
                 {/each}
